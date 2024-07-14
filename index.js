44675cc50a4649241c8488e7fdb86e28f15c73de
Vipin <vipinm922@gmail.com>
2024-07-14 23:11:02 +0530

    added ej and other configs

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
-const port = 9000;
-app.use("/", (req, res) => {
-  res.json({ message: "Hello From Express App" });
+const server = http.createServer(app);
+const io = new Server(server);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("connected: ", socket.id);
+
+  socket.on("Send-location", (data) => {
+    console.log("location id: ", socket.id);
+    io.emit("receive-location", {
+      id: socket.id,
+      ...data,
+    });
+  });
+
+  socket.on("disconnect", (data) => {
+    console.log("disconnected id: ", socket.id);
+    io.emit("disconnected-location", {
+      id: socket.id,
+      ...data,
+    });
+  });
 });
 
-app.listen(9000, () => {
-  console.log(`Starting Server on Port ${port}`);
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3100, () => {
+  console.log("Server is running on http://localhost:3100");
 });
